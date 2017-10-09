b5bd9d676bd77d536a3079699bb25343727c3e2f
rtownsend722 <rebecca.townsend722@gmail.com>
2017-10-09 12:36:04 -0700

    JSONstringify passes bare minimum requirements

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,45 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // string
+  if (typeof(obj) === 'string') {
+    return obj = '"' + obj + '"';
+  }
+  //undefined
+  if (obj === undefined || obj === null || obj === '') {
+    return 'null';
+  }
+  // boolean
+  if (typeof(obj) === 'boolean') {
+    return obj.toString();
+  }
+  // number
+  if (typeof(obj) === 'number') {
+    return obj.toString();
+  }
+  // array
+  if (Array.isArray(obj)) {
+    var result = [];
+    obj.forEach(function(item) {
+      result.push(stringifyJSON(item));   
+    });
+    return '[' + result.join(',') + ']';
+  }
+  // object
+  if (typeof(obj) === 'object') {
+    var result = [];
+    for (key in obj) {
+      if (typeof(key) === 'function' || typeof(obj[key]) == 'function' || typeof(key) === undefined || typeof(obj[key]) === undefined || key === 'undefined') {
+
+      } else {
+        result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+      
+    }
+    return '{' + result.join(',') + '}';
+  }
+  // function
+  
 };
+
+// {a: 1, b: 2, c: 3}
